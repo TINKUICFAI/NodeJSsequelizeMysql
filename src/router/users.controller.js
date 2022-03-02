9d128aa2c316d07b489ff478a1ca6ee0644e7ee2
tku739 <tinku.kumar@dotsquares.com>
2022-03-02 23:41:23 +0530

    Delete Perform

--- src/router/users.controller.js
+++ src/router/users.controller.js
@@ -51,6 +51,7 @@ function update(req, res, next) {
 }
 
 function _delete(req, res, next) {
+  console.log("req id::::::::::",req.params.id);
   userService
     .delete(req.params.id)
     .then(() => res.json({ message: "User deleted successfully" }))
